fed4789de7c5542363591e78bb2d5d4cb871fb90
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-10 23:04:21 +0300

    initialized layout

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,12 @@
-import DocumentationPage from "@app/pages/Documentation";
+import Sidebar from "@app/components/Sidebar";
 
-export default function App() {
-  return <DocumentationPage />;
-}
+const App = () => {
+  return (
+    <div className="flex h-full">
+      <Sidebar />
+      <div className="flex-1 h-full bg-gray-100 p-8">App</div>
+    </div>
+  );
+};
+
+export default App;
--- src/router/RouterErrorPage.tsx
+++ src/router/RouterErrorPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useRouteError } from "react-router-dom";
-import Layout from "@app/pages/Layout/Layout";
 
 export default function ErrorPage() {
   const error = useRouteError() as { statusText: string; message: string };
@@ -9,14 +8,13 @@ export default function ErrorPage() {
   }, []);
 
   return (
-    <Layout>
+    <div className="grid h-[100vh] w-[100%] place-items-center">
       <div id="error-page">
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
         <p>Sorry, an unexpected error has occurred.</p>
-        Link to homepage
       </div>
-    </Layout>
+    </div>
   );
 }
