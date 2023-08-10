bd7a335d86989b4cd23bb8065ed0dc4429179e15
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-11 00:07:47 +0300

    added inter font

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,24 +1,36 @@
 const Sidebar = () => {
   return (
-    <div className="h-full bg-white w-1/4 p-5 border-l border-gray-300">
+    <div className="h-full bg-white w-1/4 p-6 border-l border-gray-300">
       <div className="flex mb-4 text-2xl">BulkFake</div>
-      <div className="mb-4">
-        <label
-          htmlFor="dropdown"
-          className="block mb-1 font-medium text-gray-600"
-        >
-          Dropdown
-        </label>
-        <select
-          id="dropdown"
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-        >
-          {/* Add dropdown options here */}
-        </select>
+      <div className="mb-4 pt-10 h-2/3">
+        <div className="flex pl-[3px]">
+          <label
+            className="block text-sm font-medium text-gray-700"
+            htmlFor="region"
+          >
+            Choose a region
+          </label>
+        </div>
+        <div className="mt-1">
+          <select
+            id="region"
+            name="region"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            value=""
+          >
+            <option value="">Select a region</option>
+            <option value="north">North</option>
+            <option value="south">South</option>
+            <option value="east">East</option>
+            <option value="west">West</option>
+          </select>
+        </div>
       </div>
-      <button className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">
-        Button
-      </button>
+      <div className="flex items-end h-1/4">
+        <button className="w-full bg-[#16ad65] text-white py-2 px-4 rounded hover:bg-green-600 focus:outline-none focus:ring focus:ring-blue-300">
+          Export CVS
+        </button>
+      </div>
     </div>
   );
 };
